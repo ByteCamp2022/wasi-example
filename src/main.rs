31fc53779d6c48e319bfe850419a315c03379b3c
ERUIHNIYHBKBNF <839203521@qq.com>
2022-08-08 17:03:58 +0800

    perf: multi modules

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use wasmtime_wasi::sync::WasiCtxBuilder;
 fn main() -> Result<()> {
     // Define the WASI functions globally on the `Config`.
     let engine = Engine::default();
+    let mut linker = Linker::new(&engine);
+    wasmtime_wasi::add_to_linker(&mut linker, |s| s)?;
 
     // Create a WASI context and put it in a Store; all instances in the store
     // share this context. `WasiCtxBuilder` provides a number of ways to
@@ -22,9 +24,7 @@ fn main() -> Result<()> {
     for file in wasm_files.iter() {
         println!("Loading {}", file);
         let module = Module::from_file(&engine, file)?;
-        let mut linker = Linker::new(&engine);
-        wasmtime_wasi::add_to_linker(&mut linker, |s| s)?;
-        linker.module(&mut store, "", &module)?;
+        // linker.module(&mut store, "", &module)?;
         // linker
         //     .get_default(&mut store, "")?
         //     .typed::<(), (), _>(&store)?
